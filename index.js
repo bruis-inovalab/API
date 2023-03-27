f1d483088ae9f57d090f6a94b7df8fb74bcf1f8e
Bruis <bruis.dedaj@INOVAMANI7sMini.lan>
2023-03-27 10:38:23 +0200

    pantheon Data

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const pantheonData = require("./index.json");
+
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Pantheon Data");
+});
+
+app.get("/data", (req, res) => {
+  res.send(pantheonData);
+});
+
+app.listen(port, () => {
+  console.log(`App is listeting to  port ${port}`);
+});
